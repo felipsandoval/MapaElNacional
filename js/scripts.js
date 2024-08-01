8789bb2bf92e71f2b2ea997ca0ff2b194d7d38a9
Felipe Sandoval Sibada <felipesandoval1111@gmail.com>
2024-08-01 16:31:15 +0200

    Canada

--- js/scripts.js
+++ js/scripts.js
@@ -234,7 +234,7 @@ fetch("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/wor
             .enter().append("path")
             .attr("class", d => {
                 if (d.id === "VEN") return "country white";
-                if (["ARG", "CHL", "PER", "PAN", "CRI", "GTM", "DOM", "USA", "ECU", "PRY", "URY", "SLV", "GBR", "FRA", "DEU", "ITA", "ESP", "GRC", "TUR"].includes(d.id)) return "country blue";
+                if (["ARG", "CHL", "PER", "PAN", "CRI", "GTM", "DOM", "USA", "ECU", "PRY", "URY", "SLV", "GBR", "FRA", "DEU", "ITA", "ESP", "GRC", "TUR", "CAN"].includes(d.id)) return "country blue";
                 if (["BOL", "CUB", "NIC", "BRA", "MEX", "COL", "HND", "RUS", "BLR", "CHN", "IRN", "SRB", "SYR"].includes(d.id)) return "country red";
                 return "country grey";
             })
